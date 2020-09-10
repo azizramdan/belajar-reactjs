18577db7d7b818695193bbd8b559926f3ab7889d
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2020-09-10 15:33:50 +0700

    rendering variable pada component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Article from './Article';
+import Rendering from './Rendering';
 
 function App() {
   return (
     <div className="App">
-      <Article userLogin='Sudah login' />
+      {/* <Article userLogin='Sudah login' /> */}
+      <Rendering />
     </div>
   );
 }
