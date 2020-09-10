a40cdf17b025f9cdd7c746c0883f47eeda7d4bf8
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2020-09-10 18:26:04 +0700

    state pada component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import Event from './Event';
+import Counter from './Counter';
 
 function App() {
-  const onClick = () => window.alert('button di klik')
-
   return (
     <div className="App">
-      <Event text='klik aing' onCustomClick={onClick} />
+      <Counter />
     </div>
   );
 }
