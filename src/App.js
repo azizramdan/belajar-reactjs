39e644858556731b58240bced8e215a68fe24d67
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2020-09-10 11:25:04 +0700

    default props

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Hello name='kakak' greeting='Siang' />
+        <Hello greeting='Siang' />
       </header>
     </div>
   );
--- src/Hello.js
+++ src/Hello.js
@@ -2,4 +2,9 @@ import React from 'react'
 
 const Hello = ({name, greeting}) => <h1>Hello {name} dengan CRA, Selamat {greeting}</h1>
 
+Hello.defaultProps = {
+  name: 'World',
+  greeting: 'datang '
+}
+
 export default Hello
